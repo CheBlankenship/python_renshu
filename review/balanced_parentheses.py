0c790e98a26787a338a0f00e767fe4c6ff133c29
CheBlankenship <cnakamura336@gmail.com>
2018-05-11 14:57:20 -0500

    implemented STack Object Class and parenthises balanced cheker function

--- review/balanced_parentheses.py
+++ review/balanced_parentheses.py
@@ -14,7 +14,7 @@ class Stack:
         return None
 
     def pop(self):
-        return self.items.pop()
+        return self.items.pop(0)
 
     def is_empty(self):
         return [] == self.items
@@ -24,3 +24,45 @@ class Stack:
 
     def peek(self):
         return self.items[-1]
+
+
+
+
+def balanced_parentheses_checker(symbol_str):
+    s = Stack()
+    balanced = True
+    index = 0
+
+    while index < len(symbol_str) and balanced :
+        symbol = symbol_str[index]
+        if symbol in "({[":
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not matches(top, symbol):
+                    balanced = False
+
+        index = index +1
+
+
+    if balanced and s.is_empty():
+        return True
+    else:
+        return False
+
+
+
+def matches(top, symbol):
+    open_sym = "({["
+    close_sym = "]})"
+    return open_sym.index(top) == close_sym.index(symbol)
+
+
+
+
+
+print(balanced_parentheses_checker('{{([][])}()}'))
+print(balanced_parentheses_checker('[{()}]'))
