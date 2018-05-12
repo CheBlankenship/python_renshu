48d810c56eb852cd7d665ee344f0b62bc5706ef0
CheBlankenship <cnakamura336@gmail.com>
2018-05-12 14:55:55 -0500

    Change the task creation range depending on the student number

--- review/printer_simulation/simulation.py
+++ review/printer_simulation/simulation.py
@@ -5,16 +5,18 @@ import random
 
 
 
-def simulation(total_time, pages_per_min):
+def simulation(total_time, pages_per_min, num_of_students):
 
     lab_printer = Printer(pages_per_min) # set printer per min
     print_q = PrintQueue() # set the queue
     waiting_time = [] #
 
+
     for current_second in range(total_time):
 
         # create new print task and put it into the queue.
-        if new_print_task():
+        if new_print_task(num_of_students):
+            # print("CREATE NEW TASK")
             t = Task(current_second)
             print_q.enqueue(t)
 
@@ -25,6 +27,7 @@ def simulation(total_time, pages_per_min):
         # it will set it to busy() = True and current_task = next_task.
         # When the printer time_remaining gets 0, it will change busy() = False.
         if (not lab_printer.busy()) and (not print_q.is_empty()):
+            # print("PROCESSSING CURRENT TASK")
             next_task = print_q.dequeue()
             waiting_time.append(t.wait_time(current_second))
             lab_printer.start_next(next_task)
@@ -33,20 +36,30 @@ def simulation(total_time, pages_per_min):
         # is in the printer and sub the time it is taking.
         lab_printer.tick()
 
+
     avg_wait = sum(waiting_time)  / len(waiting_time)
     print("Average Wait %6.2f secs %3d tasks remaining." %(avg_wait, print_q.size()))
 
 
+# Range 1 - 181#
+# Number students : 10
+# Pages per task : 1 - 20
+# If there are 10 students and each prints twice, then there are 20 print tasks per hour.
+# 40/1 -> 20/
 
 
-def new_print_task():
-    rand_num = random.randrange(1, 181)
-    if rand_num == 180:
+
+def new_print_task(num_of_students):
+    # print("Range >> ", int((60*60)/(num_of_students*2)+1))
+
+    rand_num = random.randrange(1, int((60*60)/(num_of_students*2)+1))
+    if rand_num == int((60*60)/(num_of_students*2)):
         return True
     else:
         False
 
 
+# new_print_task(20)
 
 for i in range(10):
-   simulation(3600, 5)
+   simulation(3600, 15, 11)
