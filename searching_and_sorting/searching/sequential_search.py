e588305c246b9248df6f14e1b10bf01619614d27
CheBlankenship <cnakamura336@gmail.com>
2018-05-19 15:58:31 -0500

    Bsic searching

---
+++ searching_and_sorting/searching/sequential_search.py
@@ -0,0 +1,18 @@
+# O(n)
+def sequential_search(item_list, item):
+    position = 0
+    found = False
+
+    while position < len(item_list) and not found:
+        if item_list[position] == item:
+            found = True
+        else:
+            position = position + 1
+
+    return found
+
+
+
+
+arr = [1,2,3,4,5,6,7,88]
+print(sequential_search(arr, 10))
