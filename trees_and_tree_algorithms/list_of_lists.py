f59d88e8d21834df206e6fab81b4573e72dc013b
CheBlankenship <cnakamura336@gmail.com>
2018-05-24 18:40:11 -0500

    list of lists binary tree implementation

--- trees_and_tree_algorithms/list_of_lists.py
+++ trees_and_tree_algorithms/list_of_lists.py
@@ -1,22 +1,44 @@
-# List of lists
-list_tree = [1, \
-    [2, \
-        [4, \
-            [8], [11]\
-        ],\
-        [5, \
-            [9], [12]\
-        ]\
-    ], \
-    [3, \
-        [6, \
-            [10], [13] \
-        ], \
-        [7]\
-    ]\
-]
+def binary_tree(r):
+    return [r, [], []]
 
-print("Tree >> ", list_tree)
-print("Root >> ", list_tree[0])
-print("Sub tree 1 >> ", list_tree[1])
-print("Sub tree 2 >> ", list_tree[2])
+def insert_left(root, new_branch):
+    t = root.pop(1)
+    print("check >> ", t)
+    if len(t) > 1:
+        root.insert(1, [new_branch, t, [] ])
+    else:
+        root.insert(1, [new_branch, [], [] ])
+
+    return root
+
+
+def insert_right(root, new_branch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2, [new_branch, [], t])
+    else:
+        root.insert(2, [new_branch, [], []])
+
+    return root
+
+
+
+def get_root_val(root):
+    return root[0]
+
+def set_root_val(root, new_val):
+    root[0] = new_val
+    return None
+
+def get_left_child(root):
+    return root[1]
+
+def get_right_child(root):
+    return root[2]
+
+
+# lOl = binary_tree(1)
+# lOl2 = binary_tree(2)
+rot = []
+print(insert_left([1, [], []], 1))
+# print(lOl)
