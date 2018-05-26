fd83e58733f40c686a9253385716c64718bd559c
CheBlankenship <cnakamura336@gmail.com>
2018-05-26 13:36:18 -0500

    modify Binary Tree

--- trees_and_tree_algorithms/binary_tree.py
+++ trees_and_tree_algorithms/binary_tree.py
@@ -11,14 +11,57 @@ class BinaryTree:
     def __str__(self):
         return str(self.key)
 
-    def get_left_child(self):
-        return self.left_ch
+
+    def insert_left(self, new_node):
+        if self.left_ch == None:
+            self.left_ch = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.left_ch = self.left_ch
+            self.left_ch = t
+        return None
+
+    def insert_right(self, new_node):
+        if self.right_ch == None:
+            self.right_ch = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.right_ch = self.right_ch
+            self.right_ch = t
+        return None
 
     def get_right_child(self):
         return self.right_ch
 
-    def set_root_val(self, val):
-        return root.insert(1, [val, [], []])
+    def get_left_child(self):
+        return self.left_ch
+
+    def set_root_val(self, obj):
+        self.key = obj
+        return None
 
     def get_root_val(self):
         return self.key
+
+
+bt = BinaryTree(1)
+bt.insert_left(2)
+bt.insert_right(3)
+# bt.set_root_val()
+# print("root > " ,bt)
+# print("left > " ,bt.left_ch)
+# print("right > " ,bt.right_ch)
+
+# insert into pre-exist node
+bt.insert_left(4)
+# print("root > " ,bt)
+# print("left1 > " ,bt.left_ch)
+# print("right1 > " ,bt.right_ch)
+# print("left2 > " ,bt.left_ch.left_ch)
+# print("right2 > " ,bt.right_ch.right_ch)
+print(bt.get_root_val())
+print(bt.get_right_child())
+print(bt.get_left_child())
+print(bt.set_root_val("hola"))
+
+print(bt.get_root_val())
