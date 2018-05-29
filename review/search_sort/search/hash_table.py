bfcc52706275933888dddb852f9cc2be23c20fba
CheBlankenship <cnakamura336@gmail.com>
2018-05-28 23:06:11 -0500

    Review hash table put method

--- review/search_sort/search/hash_table.py
+++ review/search_sort/search/hash_table.py
@@ -1,6 +1,8 @@
 # Best case hashing : O(1)
 #
 
+
+
 class HashTable:
 
     def __init__(self):
@@ -8,7 +10,12 @@ class HashTable:
         self.slots = [None] * self.size
         self.data = [None] * self.size
 
+    def __repr__(self):
+        return str(self.slots) + "\n" + str(self.data)
+
     def hash_function(self, key, size):
+        # size が　11 の為、0 - 11 の範囲で余りが出る (index = [0] ~ [11])。
+        # よって IndexError を起こすことを防ぐことができる。
         return key % size
 
     def rehash(self, old_hash_v, size):
@@ -19,11 +26,20 @@ class HashTable:
 
         # If the slots[index] is None, link slots[index]=key and slots[index]=data
         if self.slots[hash_v] == None:
+            print("hey")
             self.slots[hash_v] = key
             self.data[hash_v] = data
         else:
+            # もしslots[index]のポジションが既に同じ値のkeyを所持していた場合、data[index]を上書きする。
+            # 例：slots[0] = 44 (44 % 11 = 4 ... 0)
             if self.slots[hash_v] == key:
+                print("OVER WRITE >> ", key)
                 self.data[hash_v] = data
+            #もしslots[index]
+            # 例：slots[0] = 44となっている為、key=77は次の空いているポジションに移動をする。
+            # 77 % 11 = 7 ... 0 and 44 % 11 = 4 ... 0
+            # slots[0] = 44
+            # slots[1] = 77
             else:
                 next_slot = self.rehash(hash_v, len(self.slots))
                 while (self.slots[next_slot] != None) and (self.slots[next_slot] != key):
@@ -35,3 +51,25 @@ class HashTable:
                 else:
                     # Replace data
                     self.data[next_slot] = data
+
+
+
+
+
+
+
+arr = [None] * 11
+# [None, None, None, None, None, None, None, None, None, None, None]
+arr[5] = "insert 1"
+arr[6] = "insert 2"
+# [None, None, None, None, None, 'insert 1', 'insert 2', None, None, None, None]
+arr[6] = "insert 3"
+
+
+h = HashTable()
+h.put(5, "insert 1")
+h.put(6, "insert 2")
+# [None, None, None, None, None, 'insert 1', 'insert 2', None, None, None, None]
+h.put(6, "insert 3")
+# h[6] = "insert 3"
+print(h)
