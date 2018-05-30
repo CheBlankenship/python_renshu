69f8cef226b90e5b17e726e099ed506decc805ed
CheBlankenship <cnakamura336@gmail.com>
2018-05-30 13:09:56 -0500

    implement bubble sort

--- review/search_sort/sort/bubble_sort.py
+++ review/search_sort/sort/bubble_sort.py
@@ -21,6 +21,20 @@ def basic_sort(item_list):
 
 
 
-
+# Buble sort .
 def bubble_sort(item_list):
+    for i in range(len(item_list) - 1, 0, -1):
+        for j in range(i):
+            if item_list[j] > item_list[j + 1]:
+                temp = item_list[j]
+                item_list[j] = item_list[j + 1]
+                item_list[j + 1] = temp
+    return item_list
+
+# item_list = [5,6,1]
+# print(bubble_sort(item_list))
+
+
+
+def short_bubble(item_list):
     pass
