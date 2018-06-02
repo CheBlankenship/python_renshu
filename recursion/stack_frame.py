e2ffb1dd2cdc8941da7b93a15e3f62c1abeedb0b
CheBlankenship <cnakamura336@gmail.com>
2018-06-02 15:07:21 -0500

    Fix the to_str() function and added test cases.

--- recursion/stack_frame.py
+++ recursion/stack_frame.py
@@ -10,8 +10,8 @@ def to_str(n, base):
     convert_string = "0123456789ABCDEF"
     #
     while n > 0:
-        if n > base:
-            stk.push(base)
+        if n < base:
+            stk.push(n)
         else:
             # Just push the remainder to the stack.
             stk.push(convert_string[n % base])
@@ -27,7 +27,7 @@ def to_str(n, base):
 
 
 
-# print(to_str(10, 2))
+#print(to_str(10000, 16))
 
 
 
@@ -42,4 +42,23 @@ def n_to_str(num, base):
         return n_to_str(num//base, base) + str(numstr[num % base])
 
 
-print(n_to_str(10, 2))
+# print(n_to_str(100, 16))
+
+def test():
+    again = 'y'
+    while again == 'y':
+        try:
+            number = int(input('Number to be converted: '))
+            base = int(input('Base to be used: '))
+        except:
+            print("Invalid input. Try again.")
+            continue
+        
+        print('Using nonrecursion')
+        print(to_str(number, base))
+        print('Using recursion')
+        print(n_to_str(number, base))
+        again = input('Do you want to  continue with another conversion? y or n: ')
+
+
+test()
