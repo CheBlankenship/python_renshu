bfbb2271114597379c7cfaefb2e9731c8862b94c
CheBlankenship <cnakamura336@gmail.com>
2018-06-08 13:53:59 -0500

    Modified binary tree modules.

--- trees_and_tree_algorithms/binary_tree_application/pars_tree/binary_tree.py
+++ trees_and_tree_algorithms/binary_tree_application/pars_tree/binary_tree.py
@@ -19,5 +19,26 @@ class BinaryTree:
             t.left_child = self.left_child
             self.left_child = t
 
+        return None
+
     def insert_right(self, new_node):
-        pass
+        if self.right_child == None:
+            self.right_child = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.right_child = self.right_child
+            self.right_child = t
+
+        return None
+
+    def get_right_child(self):
+        return str(self.right_child)
+
+    def get_left_child(self):
+        return str(self.right_child)
+
+    def set_root_value(self, obj):
+        self.key = obj
+
+    def get_root_value(self):
+        return self.key
