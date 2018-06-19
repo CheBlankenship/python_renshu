93d2b5d7111ed77c6db0a6d8688c95d523164881
CheBlankenship <cnakamura336@gmail.com>
2018-06-19 17:58:51 -0500

    Implement stack class

---
+++ trees_and_tree_algorithms/binary_tree_application/tree_traversals/stack.py
@@ -0,0 +1,28 @@
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def __repr__(self):
+        return str(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+    def push(self, item):
+        self.items.insert(0, item)
+        return None
+
+    def pop(self, pos=0):
+        if pos != 0 and int(pos) == int:
+            self.items.pop(pos)
+        else:
+            self.items.pop(0)
+
+    def peek(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return [] == self.items
+
+#
