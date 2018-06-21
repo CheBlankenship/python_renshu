cee351ef8cdae606eb1d06555ecb4c875f2ed2aa
CheBlankenship <cnakamura336@gmail.com>
2018-06-21 12:38:20 -0500

    implementing binarytree.

---
+++ trees_and_tree_algorithms/binary_tree_application/binary_tree_search/binary_search_tree.py
@@ -0,0 +1,20 @@
+from tree_node import TreeNode
+import operator
+
+class BinarySearchTree:
+
+    def __init__(self, arg):
+        self.root = None
+        self.size = 0
+
+    def length(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def put(self, key, val):
+        if self.root:
+            self._put(key, val, current_node.left_child)
+        else:
+            self.
